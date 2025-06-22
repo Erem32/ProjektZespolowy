3bf8b4d0309fb7b4287f46725d80c34783d77bd0
Mateusz-Adamczak <161976386+Mateusz-Adamczak@users.noreply.github.com>
2025-06-23 01:08:58 +0200

    Hosting: zmienna środowiskowa zamiast hardcoded localhost

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // point directly  FastAPI server
-  baseURL: 'http://localhost:8000',
+  baseURL: process.env.REACT_APP_URL || 'http://localhost:8000',
   headers: { 'Content-Type': 'application/json' },
 });
 
