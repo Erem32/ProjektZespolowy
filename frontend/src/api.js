70d87b61ed5df0c53d55c0ad76c90938faa3886b
Mateusz-Adamczak <161976386+Mateusz-Adamczak@users.noreply.github.com>
2025-06-23 22:37:09 +0200

    Hosting: [fix] poprawa odczytu przesłanych obrazów (z backendu zamiast z frontendu)

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,8 @@
 // src/api.js
 import axios from 'axios';
 
+const baseURL = process.env.REACT_APP_URL || 'http://localhost:8000';
+
 const api = axios.create({
   // point directly  FastAPI server
   baseURL: process.env.REACT_APP_URL || 'http://localhost:8000',
